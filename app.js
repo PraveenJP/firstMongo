44b3d3a8be9ddc55dc7c739c8eca27b476c2e0d5
praveenjp <roadizpraveen@gmail.com>
2016-06-05 16:21:21 +0530

    get book json feed

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ var db = 'mongodb://localhost/book';
 mongoose.connect(db);
 
 app.get('/',function(req, res){
-	console.log('Awesome mongoDB');
+	res.send('Awesome mongoDB');
+});
+
+app.get('/books',function(req,res){
+	Book.find({}).exec(function(err,books){
+		if(err){
+			res.send('Error has occured' +err);
+		}else{
+			res.json(books);
+		}
+	});
 });
 
 app.listen(PORT,function(err){
