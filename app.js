9025b116a89e51abd9b10465fa82287a4c8c3444
praveenjp <roadizpraveen@gmail.com>
2016-06-05 21:20:43 +0530

    get all and get id based

--- app.js
+++ app.js
@@ -11,16 +11,29 @@ var db = 'mongodb://localhost/book';
 
 mongoose.connect(db);
 
+// Start Domain
 app.get('/',function(req, res){
 	res.send('Awesome mongoDB');
 });
 
+// Get all books data
 app.get('/books',function(req,res){
 	Book.find({}).exec(function(err,books){
 		if(err){
 			res.send('Error has occured' +err);
 		}else{
 			res.json(books);
+		}
+	});
+});
+
+// Get Particular book detail
+app.get('/books/:id',function(req,res){
+	Book.findOne({_id:req.params.id}).exec(function(err, book){
+		if(err){
+			console.log(err);
+		}else{
+			res.json(book);
 		}
 	});
 });
